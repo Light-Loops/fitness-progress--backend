736fdd732e325fc26fe91e5e08dde6cc5edbb499
jfpanchi <jfpanchi2@espe.edu.ec>
2023-09-08 17:40:59 -0500

    Build: Initiating Project Configuration.
    In this commit, I have added the necessary depenencies, including configuration for  database.

---
+++ src/main/java/com/example/fitnessprogress/FitnessprogressApplication.java
@@ -0,0 +1,13 @@
+package com.example.fitnessprogress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessprogressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessprogressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fitnessprogress/FitnessprogressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fitnessprogress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessprogressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
